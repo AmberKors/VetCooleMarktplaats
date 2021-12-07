e6deeb7bd35991a9b7a6b131c9d05f071408a0ea
Mschuppers <m.schuppers@belastingdienst.nl>
2021-12-07 14:13:58 +0100

    [FNC-007] Registerern gebruiker Added User.java class Added UserResource.java class

--- src/main/java/nl/marktplaats/domain/Product.java
+++ src/main/java/nl/marktplaats/domain/Product.java
@@ -10,6 +10,12 @@ import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+
+//@NamedQuery(name = "product.FindOneProduct", query = "SELECT p FROM Product p where p.id = :id")
+//@NamedQuery(name = "product.FindOneUserProduct", query = "SELECT p FROM Product p where p.id = :id and p.user = :user_id")
+//@NamedQuery(name = "product.FindAll", query = "SELECT p FROM Product p")
+
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -18,11 +24,7 @@ import java.time.LocalDate;
 @NamedQueries({
         @NamedQuery(name = "Product.findAll", query = "SELECT p FROM Product p"),
         @NamedQuery(name = "Product.findOne", query = "SELECT p FROM Product p WHERE p.id=:id"),
-        // Filters in frontend of backend? (mijn voorkeur frontend)
-        @NamedQuery(name = "Product.filterOnName", query = "SELECT p from Product p WHERE p.name LIKE :str"),
-        @NamedQuery(name = "Product.filterOnDescription", query = "SELECT p from Product p WHERE p.description LIKE :str"),
-        @NamedQuery(name = "Product.filterOnPrice", query = "SELECT p from Product p WHERE p.price BETWEEN :min AND :max"),
-        // @NamedQuery(name = "Product.filterOnCategory", query = "SELECT p from Product p WHERE p.category LIKE :str"),
+
 })
 public class Product {
 
@@ -35,15 +37,12 @@ public class Product {
 
     private Category category;
 
-    @Pattern(regexp = "\\d*[.]\\d{2}")
+    @Pattern(regexp = "\\d*[.]\\d{2}?")
     private double price;
     @Size(min = 1, max = 200, message = "Maximaal 200 karakters")
     private String description;
-    //    @PastOrPresent
-    private LocalDate datePublished;
 
-    @ManyToOne
-    private User user;
+    private LocalDate datePublished;
 
 
     @Override
--- src/main/java/nl/marktplaats/resources/UserResource.java
+++ src/main/java/nl/marktplaats/resources/UserResource.java
@@ -1,10 +1,25 @@
 package nl.marktplaats.resources;
 
+import nl.marktplaats.domain.User;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 @Dependent
 public class UserResource {
 
+    @Inject
     private long id;
 
-
+    @POST
+    @Produces(APPLICATION_JSON)
+    @Consumes(APPLICATION_JSON)
+    public User post(User newUser) {return userDao.create(this.id, newUser);}
 
 }
