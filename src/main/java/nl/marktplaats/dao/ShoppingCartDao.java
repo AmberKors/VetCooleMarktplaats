acb636338366e5cada08bfcf158d6662dea91cf3
AmberKors <aa.kors@belastingdienst.nl>
2021-12-08 11:46:50 +0100

    Get ShoppingCart Products WORKSSS

--- src/main/java/nl/marktplaats/dao/ShoppingCartDao.java
+++ src/main/java/nl/marktplaats/dao/ShoppingCartDao.java
@@ -1,10 +1,14 @@
 package nl.marktplaats.dao;
 
+import nl.marktplaats.domain.Product;
 import nl.marktplaats.domain.ShoppingCart;
 
 import javax.ejb.Stateless;
+import java.util.List;
 
 @Stateless
 public class ShoppingCartDao extends Dao<ShoppingCart> {
-
+    public List<Product> getWithProducts(ShoppingCart shoppingCart) {
+        return shoppingCart.getProducts();
+    }
 }
--- src/main/java/nl/marktplaats/resources/ShoppingCartResource.java
+++ src/main/java/nl/marktplaats/resources/ShoppingCartResource.java
@@ -1,11 +1,13 @@
 package nl.marktplaats.resources;
 
 import nl.marktplaats.dao.ShoppingCartDao;
+import nl.marktplaats.domain.Product;
 import nl.marktplaats.domain.ShoppingCart;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -18,8 +20,9 @@ public class ShoppingCartResource {
 
     @GET
     @Produces(APPLICATION_JSON)
-    public ShoppingCart getOneShoppingCartById(@PathParam("cartId") int id) {
-        return shoppingCartDao.getById(id);
+    public List<Product> getWithProducts(@PathParam("cartId") int id) {
+        ShoppingCart shoppingCart = shoppingCartDao.getById(id);
+        return shoppingCartDao.getWithProducts(shoppingCart);
     }
 
     @PUT
