62be9ba3f5893fe43c54f2fb0884e2d288c45b88
AmberKors <aa.kors@belastingdienst.nl>
2021-12-09 15:54:04 +0100

    [STRY-008] Create shoppingcart when creating new user

--- src/main/java/nl/marktplaats/resources/UsersResource.java
+++ src/main/java/nl/marktplaats/resources/UsersResource.java
@@ -1,12 +1,13 @@
 package nl.marktplaats.resources;
 
+import nl.marktplaats.dao.ShoppingCartDao;
 import nl.marktplaats.dao.UserDao;
+import nl.marktplaats.domain.ShoppingCart;
 import nl.marktplaats.domain.User;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
-
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -16,6 +17,9 @@ public class UsersResource {
 
     @Inject
     private UserDao userDao;
+
+    @Inject
+    private ShoppingCartDao shoppingCartDao;
 
     @Inject
     private UserResource userResource;
@@ -33,6 +37,8 @@ public class UsersResource {
     @Produces(APPLICATION_JSON)
     @Operation(description = "Register new user to User-DBtable")
     public User add(User u) {
+        ShoppingCart cart = shoppingCartDao.add(new ShoppingCart());
+        u.setShoppingCart(cart);
         return userDao.add(u);
     }
 
